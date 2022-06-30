1c46b06f48c08c7c89859fe1a497a62095b46993
Kristian Lalov <94289812+kris34@users.noreply.github.com>
2022-06-30 20:44:38 +0300

    Add files via upload

---
+++ Advanced/Unit testing Exercise part 2 /06.Dynamic-Validation/app.js
@@ -0,0 +1,13 @@
+function validate() {
+  let input = document.getElementById("email");
+
+  let pattern = new RegExp("^[a-z]+@[a-z]+\\.[a-z]+");
+
+  input.addEventListener("change", () => {
+    if (pattern.test(input.value)) {
+      input.classList.remove("error");
+    } else {
+      input.classList.add("error");
+    }
+  });
+}
